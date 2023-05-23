77c7897be34deffc4188cf9002fe1ea82e62eb06
onuraycicek <onur.m.aycicek@gmail.com>
2023-05-23 22:56:10 +0300

    added the feature of listing users to the panel

--- app/Http/Controllers/Dashboard/Admin/UsersController.php
+++ app/Http/Controllers/Dashboard/Admin/UsersController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Dashboard\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function show()
     {
-        return view('dashboard.admin.users');
+        $users = User::latest()->paginate(10);
+        return view('dashboard.admin.users', [
+            'users' => $users
+        ]);
     }
 }
--- resources/views/dashboard/admin/users.blade.php
+++ resources/views/dashboard/admin/users.blade.php
@@ -1,3 +1,40 @@
 <x-dashboard-layout>
-    <x-dashboard.admin.user-list />
+    <div class=" p-8 rounded-md w-full">
+        <div>
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div class="inline-block w-full rounded-lg overflow-hidden">
+                    <table class="w-full leading-normal">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Email
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                        <p class="text-gray-900 whitespace-no-wrap">
+                                            {{ $user->name }}
+                                        </p>
+                                    </td>
+                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                        <p class="text-gray-900 whitespace-no-wrap">{{ $user->email }}</p>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{ $users->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+
 </x-dashboard-layout>
